82caff46734698c51c747c3b97488c06ce192639
Haider <haiderabbas1982000@gmail.com>
2020-08-14 18:04:09 +0500

    Added player.png onto game screen

--- main.py
+++ main.py
@@ -13,12 +13,26 @@ pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load("rocket.png")
 pygame.display.set_icon(icon)
 
+# Player
+playerImg = pygame.image.load("player.png")
+playerImg = pygame.transform.scale(playerImg, (64, 64))
+playerX = 370
+playerY = 480
+
+
+def player():
+    screen.blit(playerImg, (playerX, playerY))
+
+
 # Game loop
 running = True
 while running:
+    # background color
+    screen.fill((0, 0, 128))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    # background color
-    screen.fill((0, 0, 128))
+
+    player()
     pygame.display.update()
