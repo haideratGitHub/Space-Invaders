bb3005d7760e8c24d4ee57cd54e3a41e0b309565
Haider <haiderabbas1982000@gmail.com>
2020-08-14 17:05:53 +0500

    changed title, logo and background color

--- main.py
+++ main.py
@@ -6,9 +6,19 @@ pygame.init()
 # creating the game window
 screen = pygame.display.set_mode((800, 600))
 
+# Title
+pygame.display.set_caption("Space Invaders")
+
+# Icon
+icon = pygame.image.load("rocket.png")
+pygame.display.set_icon(icon)
+
 # Game loop
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    # background color
+    screen.fill((0, 0, 128))
+    pygame.display.update()
